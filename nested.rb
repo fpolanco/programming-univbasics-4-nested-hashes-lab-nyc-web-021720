96a0b760b2c396fa01f1eb7501ca2791c0f711e4
Franchell Polanco <polanco.franchell@gmail.com>
2020-01-22 19:55:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -6,16 +6,17 @@ def hopper
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
+        # :alan_kay => {
+        #   :known_for => "Object Orientation",
+        #   :languages => ["Smalltalk", "LISP"]
+        # },
+        # :dennis_ritchie => {
+        #   :known_for => "Unix",
+        #   :languages => ["C"]
+        # }
+
      }
-  
+
 
 end
 
